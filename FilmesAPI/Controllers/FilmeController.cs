d294e23b7d22727c7937f33f92e5376778ebfe64
Danton da Rosa Abreu <dantondev@gmail.com>
2025-02-17 22:09:49 -0300

    Add Model and Controller Create methods Get and Post

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new();
+
+        [HttpPost]
+        public void PostFilme(Filme filme)
+        {
+            filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> GetFilme()
+        {
+            return filmes;
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [MaxLength(500)]
+        public String Titulo { get; set; }
+
+        [Required(ErrorMessage = "A duração é obrigatória.")]
+        [Range(30, 280, ErrorMessage = "A duração deve ser entre 30 minutos e 280 minutos.")]
+        public String Duracao { get; set; }
+
+        [Required(ErrorMessage = "O Genêro é obrigatório.")]
+        public String Genero { get; set; }
+    }
+}
